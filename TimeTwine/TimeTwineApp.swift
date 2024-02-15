21a18d1f7103ece66514484abbaff9bec192163e
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-02-15 12:34:41 -0800

    Initial Commit

---
+++ TimeTwine/TimeTwineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTwineApp.swift
+//  TimeTwine
+//
+//  Created by Nikolai Nobadi on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeTwineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
